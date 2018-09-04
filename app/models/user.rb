c292a26a7ed72265c20694aab9c0ef32d82dc03d
soartec-lab <shodai-connect@tbz.t-com.ne.jp>
2018-09-04 15:31:26 +0900

    認証に成功してコールバックしてきた際に行う処理を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :omniauthable
+  def self.find_or_create_with_doorkeeper(auth)
+    user = self.find_by(provider: auth.provider, uid: auth.uid )
+    return user unless user.nil?
+
+    self.create(
+      email: auth.info.user.email,
+      provider: auth.provider,
+      uid: auth.uid,
+      password: Devise.friendly_token[0, 20]
+    )
+  end
 end
